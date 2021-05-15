78844a7b3f899657ab4f65f62e05243d33479714
Eugene Glushankov <emglus@gmail.com>
2021-05-16 08:21:13 +1000

    update to index.html (and js)

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function updateMemberList() {
-    let tableHeadRow = "<tr><th>Username</th><th>Level</th><th>Counter</th></tr>";
+    let tableHeadRow = "<tr><th>Имя</th><th>Уровень</th></tr>";
     let buttonHead = '<button class="ng-button-small">';
     let buttonTail = '</button>';
     
@@ -10,12 +10,18 @@ function updateMemberList() {
         let cell1 = document.createElement("td");
         cell1.innerHTML = buttonHead + members[i].name + buttonTail;
         row.appendChild(cell1);
+
         let cell2 = document.createElement("td");
-        cell2.innerHTML = members[i].level.toString();
+        cell2.innerHTML = displayLevel(members[i].level, members[i].counter)
         row.appendChild(cell2);
-        let cell3 = document.createElement("td");
-        cell3.innerHTML = members[i].counter.toString();
-        row.appendChild(cell3);
         memList.appendChild(row);
     }
+}
+
+function displayLevel(level, counter) {
+    let part1 = '<span class="w3-badge w3-xlarge">' + level.toString() + '</span>'
+    for (let i = 0; i < counter; i++) {
+        part1 += '<i class="material-icons">star</i>'
+    }
+    return part1;
 }
