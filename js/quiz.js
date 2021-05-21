8271a2b5e252c69e88917fd5bac4bb5721c1d4fc
Eugene Glushankov <emglus@gmail.com>
2021-05-21 22:54:20 +1000

    matching level number to the level itself

--- js/quiz.js
+++ js/quiz.js
@@ -1,176 +1,66 @@
-//import {level01} from './quizes/level01.js';
-//import {level02} from './quizes/level02.js';
-
 function getQuizByLevelNumber(levelNum) {
-    let quiz = {
-        title: "",
-        timePerProblem: 3,
-        getAllProblems: function () {
-        },
-        getProblemsToSolve: function () {
-        }
-    }
-
     switch (levelNum) {
         case 1:
-            // quiz.title = level01.title;
-            // quiz.timePerProblem = level01.timePerProblem;
-            // quiz.getProblemsToSolve = level01.getProblemsToSolve;
-            // break;
             return level01;
         case 2:
-            // quiz.title = level02.title;
-            // quiz.timePerProblem = level02.timePerProblem;
-            // quiz.getProblemsToSolve = level02.getProblemsToSolve;
-            // break;
             return level02;
         case 3:
-            quiz.title = level03.title;
-            quiz.timePerProblem = level03.timePerProblem;
-            quiz.getProblemsToSolve = level03.getProblemsToSolve;
-            break;
+            return level03;
         case 4:
-            quiz.title = level04.title;
-            quiz.timePerProblem = level04.timePerProblem;
-            quiz.getProblemsToSolve = level04.getProblemsToSolve;
-            break;
+            return level04;
         case 5:
-            quiz.title = level05.title;
-            quiz.timePerProblem = level05.timePerProblem;
-            quiz.getProblemsToSolve = level05.getProblemsToSolve;
-            break;
+            return level05;
         case 6:
-            quiz.title = level06.title;
-            quiz.timePerProblem = level06.timePerProblem;
-            quiz.getProblemsToSolve = level06.getProblemsToSolve;
-            break;
+            return level06;
         case 7:
-            quiz.title = level07.title;
-            quiz.timePerProblem = level07.timePerProblem;
-            quiz.getProblemsToSolve = level07.getProblemsToSolve;
-            break;
+            return level07;
         case 8:
-            quiz.title = level08.title;
-            quiz.timePerProblem = level08.timePerProblem;
-            quiz.getProblemsToSolve = level08.getProblemsToSolve;
-            break;
+            return level08;
         case 9:
-            quiz.title = level09.title;
-            quiz.timePerProblem = level09.timePerProblem;
-            quiz.getProblemsToSolve = level09.getProblemsToSolve;
-            break;
+            return level09;
         case 10:
-            quiz.title = level10.title;
-            quiz.timePerProblem = level10.timePerProblem;
-            quiz.getProblemsToSolve = level10.getProblemsToSolve;
-            break;
+            return level10;
         case 11:
-            quiz.title = level11.title;
-            quiz.timePerProblem = level11.timePerProblem;
-            quiz.getProblemsToSolve = level11.getProblemsToSolve;
-            break;
+            return level11;
         case 12:
-            quiz.title = level12.title;
-            quiz.timePerProblem = level12.timePerProblem;
-            quiz.getProblemsToSolve = level12.getProblemsToSolve;
-            break;
+            return level12;
         case 13:
-            quiz.title = level13.title;
-            quiz.timePerProblem = level13.timePerProblem;
-            quiz.getProblemsToSolve = level13.getProblemsToSolve;
-            break;
+            return level13;
         case 14:
-            quiz.title = level14.title;
-            quiz.timePerProblem = level14.timePerProblem;
-            quiz.getProblemsToSolve = level14.getProblemsToSolve;
-            break;
+            return level14;
         case 15:
-            quiz.title = level15.title;
-            quiz.timePerProblem = level15.timePerProblem;
-            quiz.getProblemsToSolve = level15.getProblemsToSolve;
-            break;
-                
+            return level15;
         case 16:
-            quiz.title = level16.title;
-            quiz.timePerProblem = level16.timePerProblem;
-            quiz.getProblemsToSolve = level16.getProblemsToSolve;
-            break;
+            return level16;
         case 17:
-            quiz.title = level17.title;
-            quiz.timePerProblem = level17.timePerProblem;
-            quiz.getProblemsToSolve = level17.getProblemsToSolve;
-            break;
+            return level17;
         case 18:
-            quiz.title = level18.title;
-            quiz.timePerProblem = level18.timePerProblem;
-            quiz.getProblemsToSolve = level18.getProblemsToSolve;
-            break;
+            return level18;
         case 19:
-            quiz.title = level19.title;
-            quiz.timePerProblem = level19.timePerProblem;
-            quiz.getProblemsToSolve = level19.getProblemsToSolve;
-            break;
- 
+            return level19;
         case 20:
-            quiz.title = level20.title;
-            quiz.timePerProblem = level20.timePerProblem;
-            quiz.getProblemsToSolve = level20.getProblemsToSolve;
-            break;
+            return level20;
         case 21:
-            quiz.title = level21.title;
-            quiz.timePerProblem = level21.timePerProblem;
-            quiz.getProblemsToSolve = level21.getProblemsToSolve;
-            break;
+            return level21;
         case 22:
-            quiz.title = level22.title;
-            quiz.timePerProblem = level22.timePerProblem;
-            quiz.getProblemsToSolve = level22.getProblemsToSolve;
-            break;
+            return level22;
         case 23:
-            quiz.title = level23.title;
-            quiz.timePerProblem = level23.timePerProblem;
-            quiz.getProblemsToSolve = level23.getProblemsToSolve;
-            break;
+            return level23;
         case 24:
-            quiz.title = level24.title;
-            quiz.timePerProblem = level24.timePerProblem;
-            quiz.getProblemsToSolve = level24.getProblemsToSolve;
-            break;
+            return level24;
         case 25:
-            quiz.title = level25.title;
-            quiz.timePerProblem = level25.timePerProblem;
-            quiz.getProblemsToSolve = level25.getProblemsToSolve;
-            break;
+            return level25;
         case 26:
-            quiz.title = level26.title;
-            quiz.timePerProblem = level26.timePerProblem;
-            quiz.getProblemsToSolve = level26.getProblemsToSolve;
-            break;
+            return level26;
         case 27:
-            quiz.title = level27.title;
-            quiz.timePerProblem = level27.timePerProblem;
-            quiz.getProblemsToSolve = level27.getProblemsToSolve;
-            break;
+            return level27;
         case 28:
-            quiz.title = level28.title;
-            quiz.timePerProblem = level28.timePerProblem;
-            quiz.getProblemsToSolve = level28.getProblemsToSolve;
-            break;
+            return level28;
         case 29:
-            quiz.title = level29.title;
-            quiz.timePerProblem = level29.timePerProblem;
-            quiz.getProblemsToSolve = level29.getProblemsToSolve;
-            break;
+            return level29;
         case 30:
-            quiz.title = level30.title;
-            quiz.timePerProblem = level30.timePerProblem;
-            quiz.getProblemsToSolve = level30.getProblemsToSolve;
-            break;
-                    
-                
+            return level30;
         default:
-            break;                        
+            return null;
     }
-
-    return quiz;
 }
